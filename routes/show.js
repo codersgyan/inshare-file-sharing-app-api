4863bcaa636ec70735f37d5314a12e3cc9f71bcf
Rakesh K <codersgyan@gmail.com>
2020-08-21 01:42:45 +0300

    change routes

---
+++ routes/show.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const File = require('../models/file');
+
+router.get('/:uuid', async (req, res) => {
+    try {
+        const file = await File.findOne({ uuid: req.params.uuid });
+        // Link expired
+        if(!file) {
+            return res.render('download', { error: 'Link has been expired.'});
+        } 
+        return res.render('download', { uuid: file.uuid, fileName: file.filename, fileSize: file.size, downloadLink: `${process.env.APP_BASE_URL}/download/${file.uuid}` });
+    } catch(err) {
+        return res.render('download', { error: 'Something went wrong.'});
+    }
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -24,11 +24,13 @@ connectDB();
 
 app.use(express.json());
 
-app.set('views', path.join(__dirname, '/views'))
-app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
 
 // Routes 
-app.use('/api/files', require('./routes/files'))
-app.use('/files', require('./routes/downloader'))
+app.use('/api/files', require('./routes/files'));
+app.use('/files', require('./routes/show'));
+app.use('/download', require('./routes/download'));
+
 
 app.listen(PORT, console.log(`Listening on port ${PORT}.`));
